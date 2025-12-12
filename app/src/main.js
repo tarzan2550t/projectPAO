064284be4e5f8b0853b9a7eec5cee588275af00f
Danupon723 <danuponmaneesing@gmail.com>
2025-12-12 14:31:48 +0700

    xcz

---
+++ app/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'vuetify/styles'
+import router from './router'
+
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives
+})
+
+createApp(App)
+.use(router)
+.use(vuetify)
+.mount('#app')
---
+++ app/src/view/adminlayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-title>ยินดีต้อนรับเเอดมิน</v-app-bar-title>
+        </v-app-bar>
+    </v-app>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
